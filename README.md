53b4c8e05e72185033fab7343370bae5a7f834c8
weimingtom <weimingtom@qq.com>
2016-08-02 10:08:41 +0800

    ready

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-twentylight
+﻿twentylight
 ===========
 
 My experimental game IDE(former name twilight). It's only a very simple demo.
+
+## Ref  
+* Vuhdo/ddsview  
+https://github.com/Vuhdo/ddsview  
+http://www.amnoid.de/ddsview/download.html  
+
+* DevIL Downloads (Developer's Image Library)    
+http://openil.sourceforge.net/download.php  
+https://sourceforge.net/projects/openil/files/  
+
+* Simple Browse  
+Simple Browse-简洁实用的图片浏览器  
+
--- vc6/clean_all.bat
+++ vc6/clean_all.bat
@@ -3,15 +3,15 @@ set ROOT_PATH=%cd%
 
 cd %ROOT_PATH%\zlib
 rmdir /S /Q Debug Release
-del /S /Q *.ncb *.opt *.suo *.exe *.aps
+del /S /Q *.ncb *.opt *.suo *.exe *.aps *.plg
 
 cd %ROOT_PATH%\libpng
 rmdir /S /Q Debug Release
-del /S /Q *.ncb *.opt *.suo *.exe *.aps
+del /S /Q *.ncb *.opt *.suo *.exe *.aps *.plg
 
 cd %ROOT_PATH%
 rmdir /S /Q Debug Release
-del /S /Q *.ncb *.opt *.suo *.exe *.aps
+del /S /Q *.ncb *.opt *.suo *.exe *.aps *.plg
 
 ::pause
 @echo on
